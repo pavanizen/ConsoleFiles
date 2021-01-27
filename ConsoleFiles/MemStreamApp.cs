313b04eb3a8a69ac2ab7c7c8bf057f3bc49cd517
Pavani.Reddy <pavani.veeramreddy@zensar.com>
2021-01-27 10:02:46 +0530

    Add MemoryStream Class

---
+++ ConsoleFiles/MemStreamApp.cs
@@ -0,0 +1,29 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.IO;
+using System.Diagnostics;
+
+namespace ConsoleFiles
+{
+    class MemStreamApp
+    {
+        static void Main()
+        {
+            var t1 = Stopwatch.StartNew();
+            // Use MemoryStream directly with no buffering.
+            using (MemoryStream memory = new MemoryStream())
+            {
+                // Write a byte 5 million times.
+                for (int i = 0; i < 5000000; i++)
+                {
+                    memory.WriteByte(5);
+                }
+            }
+            t1.Stop();
+            Console.WriteLine("MEMORYSTREAM TIME: " + t1.Elapsed.TotalMilliseconds);
+        }
+    }
+}
